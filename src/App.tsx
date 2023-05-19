e0b740c65a98df6663b41eca726a939038cd345c
Hai Longe <hailongkc1203@example.com>
2023-05-20 02:10:43 +0700

    Setup Path Alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Portfolio from "./screens/portfolio";
-import "./styles/plugins.scss";
-import "./styles/style.scss";
+import Portfolio from "@/screens/portfolio";
+import "@/styles/plugins.scss";
+import "@/styles/style.scss";
 
 function App() {
   const [count, setCount] = useState(0);

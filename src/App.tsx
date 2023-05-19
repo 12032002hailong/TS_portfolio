25cd5dce1c36eec1b5954e4f9fe579eb591a8821
Hai Longe <hailongkc1203@example.com>
2023-05-20 01:56:51 +0700

    setup SASS

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Portfolio from "./screens/portfolio";
+import "./styles/plugins.scss";
+import "./styles/style.scss";
 
 function App() {
   const [count, setCount] = useState(0);
